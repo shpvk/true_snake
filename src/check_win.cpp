2ba81aeecf1204e235f48b4a5ffd8aad6e753548
shpvk <holotovegor99@gmail.com>
2025-05-28 22:29:47 +0300

    added include check_win in main func

--- src/check_win.cpp
+++ src/check_win.cpp
@@ -1,9 +1,10 @@
-#include "includes.h"
-#include  "check_win.h"
+#include    "includes.h"
+#include    "check_win.h"
 
 bool check_win(int score, int target_score) {
     if (score >= target_score) {
         std::cout << "YOU WIN! You ate: " << score << " apples.\n";
+        system("PAUSE");
         return true;
     }
     return false;
